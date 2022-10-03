5ae128620b24d50e8dd89a19d005a379a2cb6846
Hunter-Mayer <hmayer95@gmail.com>
2022-10-03 11:54:22 -0500

    Added color based off current time

--- script.js
+++ script.js
@@ -3,3 +3,19 @@ var currentTime = document.getElementById('currentDay')
 var time = moment().format('MMMM Do YYYY, h:mm:ss a')
 currentTime.innerText = time
 var saveBtnEl = document.querySelectorAll('saveBtn')
+var text = $(".text-area")
+
+//Sets colors of time block for present, future, past times
+text.each(function(){
+    var rows=$(this).attr('id')
+    var currentTime = moment().hour()
+        if (rows < currentTime){
+            $(this).addClass("past")
+        } else if (rows > currentTime){
+            $(this).addClass("future")
+        } else {
+            $(this).addClass("present")
+        }
+})
+    
+
